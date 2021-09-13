a9aade8e79545dce55c38e9d0436fa95c4beeaf4
AlahiraJ <alahirajeffrey@gmail.com>
2021-09-13 21:07:58 +0100

    code refactor

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -4,18 +4,19 @@ const router = require('express').Router()
 module.exports = () => {
 
     //create new note
-    router.post('/notes', notes.create);
+    router.post('/notes/create', notes.create)
     
     //retrieve all notes
-    router.get('/notes', notes.findAll)
+    router.get('/notes/findAll', notes.findAll)
     
     //delete note
-    router.delete('/notes', notes.delete)
+    router.delete('/notes/delete', notes.delete)
     
     //retrieve single note
-    router.get('/notes', notes.findOne)
+    router.get('/notes/findOne', notes.findOne)
     
     //update notes
-    router.put('/notes', notes.update)
+    router.put('/notes/update', notes.update)
 
+    return router
 }

b7fce474397bda1341b6d3a703df3a126a59804f
AlahiraJ <alahirajeffrey@gmail.com>
2021-09-13 21:08:20 +0100

    code refactor

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const dbConfig = require('./config/database.config')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const notesRoutes = require('./routes/notes.routes')
+const router = notesRoutes()
 
 //create express app
 const app = express()
 
 //app configuration
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(router)
 
 //setup database connection
 mongoose.connect(dbConfig.url,{

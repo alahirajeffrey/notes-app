1696e0cbc6421b49354990c9903e50369715e3f0
AlahiraJ <alahirajeffrey@gmail.com>
2021-09-13 19:11:26 +0100

    refactored code

--- models/notes.model.js
+++ models/notes.model.js
@@ -8,6 +8,10 @@ const NoteSchema = mongoose.Schema({
     note : {
         type : String,
         required : true
+    },
+    dateCreated : {
+        type : Date,
+        default : Date.now()
     }
 })
 
